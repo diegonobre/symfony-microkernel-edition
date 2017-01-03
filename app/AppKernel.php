9c518c9e24ff8741d19122f344ebc48a5109070f
Diego Nobre <dcnobre@gmail.com>
2017-01-02 23:52:21 -0300

    First commit inspired by Symfony 3.2 MicroKernelTrait documentation

---
+++ app/AppKernel.php
@@ -0,0 +1,71 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+// require Composer's autoloader
+$loader = require __DIR__.'/../vendor/autoload.php';
+// auto-load annotations
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+class AppKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle()
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+        }
+
+        return $bundles;
+    }
+
+    protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config.yml');
+
+        // configure WebProfilerBundle only if the bundle is enabled
+        if (isset($this->bundles['WebProfilerBundle'])) {
+            $c->loadFromExtension('web_profiler', array(
+                'toolbar' => true,
+                'intercept_redirects' => false,
+            ));
+        }
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        // import the WebProfilerRoutes, only if the bundle is enabled
+        if (isset($this->bundles['WebProfilerBundle'])) {
+            $routes->import('@WebProfilerBundle/Resources/config/routing/wdt.xml', '/_wdt');
+            $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml', '/_profiler');
+        }
+
+        // load the annotation routes
+        $routes->import(__DIR__.'/../src/App/Controller/', '/', 'annotation');
+    }
+
+    // optional, to use the standard Symfony cache directory
+    public function getCacheDir()
+    {
+        return __DIR__.'/../var/cache/'.$this->getEnvironment();
+    }
+
+    // optional, to use the standard Symfony logs directory
+    public function getLogDir()
+    {
+        return __DIR__.'/../var/logs';
+    }
+}
+
